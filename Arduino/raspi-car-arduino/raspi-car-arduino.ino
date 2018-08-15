08c8d3f82b12b503081f808a68f6d9658a3ff326
Xine <lostxine@gmail.com>
2018-08-15 18:54:22 +0800

    fix arduino gpio bug

--- Arduino/raspi-car-arduino/raspi-car-arduino.ino
+++ Arduino/raspi-car-arduino/raspi-car-arduino.ino
@@ -175,8 +175,7 @@ void reset_all(){
 
 // check raspi enable GPIO
 bool check_enable(){
-  bool enable = analogRead(RPI_ENABLE) > 600;
-  //bool enable = true;
+  bool enable = digitalRead(RPI_ENABLE);
   digitalWrite(LED_BUILTIN, enable);
   return enable;
 }
